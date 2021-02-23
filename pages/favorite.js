789df37304e865180e979f59e695ff510c03a222
yamayamaKo <yamachanwow@gmail.com>
2021-02-23 18:27:57 +0900

    add:お気に入り表示ページを追加

---
+++ pages/favorite.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Layout from '../../components/Layout'
+
+export default () => {
+    return(
+        <Layout title="favorite page">
+            <h1>お気に入り</h1>
+            <h1>おすすめの企画</h1>
+            <h1>閲覧した企画</h1>
+        </Layout>
+    )
+}
