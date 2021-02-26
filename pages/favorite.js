bd765c5772f230dc73186ea6b0b6fe8fd00c0706
roaris <one0803kyopro@gmail.com>
2021-02-26 11:37:33 +0900

    edit: favorite.js favorite.jsの変更

--- pages/favorite.js
+++ pages/favorite.js
@@ -1,12 +1,11 @@
 import Link from 'next/link'
-import Layout from '../../components/Layout'
+import Layout from '../components/Layout'
+import Favorite from '../components/Favorite'
 
 export default () => {
     return(
         <Layout title="favorite page">
-            <h1>お気に入り</h1>
-            <h1>おすすめの企画</h1>
-            <h1>閲覧した企画</h1>
+            <Favorite />
         </Layout>
     )
 }
