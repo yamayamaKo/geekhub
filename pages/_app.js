4da14368c31ecd3e308c0b8c49591198c0954c15
yamayamaKo <yamachanwow@gmail.com>
2021-02-27 14:40:08 +0900

    add:reduxのpersist化

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,43 @@
 import App, {Container} from 'next/app';
-import React from 'react';
+// import React from 'react';
 import withReduxStore from '../lib/redux-store';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react/'
 import '../styles/globals.css'
 import '../styles/EventCard.css'
 import '../styles/Recommendation.css'
 import '../styles/Favorite.css'
 import '../src/tailwind.css'
 
+// export default function App({ Component, pageProps }) {
+//   const store = useStore(pageProps.initialReduxState)
+//   const persistor = persistStore(store, {}, function(){
+//     persistor.persist()
+//   })
+
+//   return(
+//     <Container>
+//       <Provider store={store}>
+//         <PersistGate loading={<div>loading</div>} persistor={persistor}>
+//           <Component {...pageProps} />
+//         </PersistGate>
+//       </Provider>
+//     </Container>
+//   )
+// }
+
 class _App extends App {
   render () {
     const {Component, pageProps, reduxStore} = this.props
+    const persistedStore = persistStore(reduxStore)
+
     return (
       <Container>
         <Provider store={reduxStore}>
-          <Component {...pageProps} />
+          <PersistGate loading={<p>now loading...</p>} persistor={persistedStore}>
+            <Component {...pageProps} />
+          </PersistGate>
         </Provider>
       </Container>
     )
