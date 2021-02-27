3156b198499ec98cc80df7e27cf99ee2b7bc18e0
roaris <one0803kyopro@gmail.com>
2021-02-28 04:31:04 +0900

    fixed store.js 環境変数を利用するように変更

--- store.js
+++ store.js
@@ -2,14 +2,19 @@ import {createStore, applyMiddleware} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import firebase from 'firebase/app'
 import 'firebase/database'
-import firebaseConfig from './api_key'
 import { useMemo } from 'react'
 
 import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage/'
 
-let store
-var fireapp
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+}
 try {
     firebase.initializeApp(firebaseConfig)
 }
