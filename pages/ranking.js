b437467199487c092df7941c7dcb71968706067d
yamayamaKo <yamachanwow@gmail.com>
2021-02-27 22:40:29 +0900

    add:ランキングページを追加。また、お気に入りをマイページに変更

---
+++ pages/ranking.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import Ranking from '../components/Ranking'
+
+export default () => {
+    return(
+        <Layout title="ranking page">
+            <Ranking />
+        </Layout>
+    )
+}
