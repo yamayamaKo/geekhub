7d4b355eb9b2be97d8be40e552566531b309b5f5
yamayamaKo <yamachanwow@gmail.com>
2021-02-28 18:34:13 +0900

    edit:w-auto h-autoにしてケータイでもバグらないかな

--- components/Introduction.js
+++ components/Introduction.js
@@ -17,24 +17,10 @@ const Introduction = () => {
                     </div>
 
                     <div className='flex justify-center lg:justify-start mx-auto lg:pl-4'>
-                        <img className='rounded-lg h-auto' src='../static/design_images/tomei.jpg' width='450' height='auto' />
-                        {/* <img src='../static/design_images/example-22.svg' style={{objectFit:'contain'}} width='400' height='250'/> */}
+                        <img className='rounded-lg w-auto h-auto' src='../static/design_images/tomei.jpg'/>
                     </div>
                 </div>
             </section>
-            {/* <div className='grid md:grid-cols-2 py-8 px-4'>
-                <div>
-                    <h1>あなただけの技育祭をGeekHubで</h1>
-                    <p>
-                        GeekHubは、エンジニアを目指す学生のためのテックカンファレンスである<a href='https://talent.supporterz.jp/geeksai/2021/'>技育祭</a>
-                        での体験をより良いものにするアプリです。
-                    </p>
-                </div>
-
-                <div className='justify-center mx-auto lg:pl-4'>
-                    <img src='../static/design_images/womenTopPage.jpg' width='420' height='250' />
-                </div>
-            </div> */}
         </div>
     )
 }
